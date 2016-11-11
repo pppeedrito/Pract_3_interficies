bc1b122254f4339853a6e1d7c2e6c95637178f0f
pppeedrito <pedro1996616@gmail.com>
2016-11-11 21:40:34 +0100

    p

--- practica3_interficies/practica3_interficies/Form1.vb
+++ practica3_interficies/practica3_interficies/Form1.vb
@@ -1,9 +1,14 @@
 ﻿Public Class Form1
     Private Sub BtnAgr_Click(sender As Object, e As EventArgs) Handles BtnAgr.Click
-
+        If (ComboObligatorio.SelectedIndex = 0) Then
+            Lista.Items.Insert(0, Text)
+        ElseIf (ComboObligatorio.SelectedIndex = 0) Then
+            Lista.Items.Add(Text)
+        End If
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ComboObligatorio.SelectedIndex = 1
     End Sub
+
 End Class
