d5c6fce0af34ed5d30c6c6ca9d8eba50d747eb75
pppeedrito <pedro1996616@gmail.com>
2016-11-11 17:45:59 +0100

    casi

--- practica3_interficies/practica3_interficies/Form1.Designer.vb
+++ practica3_interficies/practica3_interficies/Form1.Designer.vb
@@ -22,16 +22,17 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.Button1 = New System.Windows.Forms.Button()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Lista = New System.Windows.Forms.ListBox()
+        Me.TxtUsuari = New System.Windows.Forms.TextBox()
+        Me.BtnAgr = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Button4 = New System.Windows.Forms.Button()
         Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboObligatorio = New System.Windows.Forms.ComboBox()
         Me.Button5 = New System.Windows.Forms.Button()
         Me.Button6 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
@@ -45,58 +46,58 @@ Partial Class Form1
         Me.Button1.Text = "Preguntar"
         Me.Button1.UseVisualStyleBackColor = True
         '
-        'ListBox1
+        'Lista
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 16
-        Me.ListBox1.Location = New System.Drawing.Point(29, 64)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(289, 228)
-        Me.ListBox1.TabIndex = 1
+        Me.Lista.FormattingEnabled = True
+        Me.Lista.ItemHeight = 16
+        Me.Lista.Location = New System.Drawing.Point(29, 64)
+        Me.Lista.Name = "Lista"
+        Me.Lista.Size = New System.Drawing.Size(289, 228)
+        Me.Lista.TabIndex = 1
         '
-        'TextBox1
+        'TxtUsuari
         '
-        Me.TextBox1.Location = New System.Drawing.Point(352, 50)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(213, 22)
-        Me.TextBox1.TabIndex = 2
+        Me.TxtUsuari.Location = New System.Drawing.Point(352, 50)
+        Me.TxtUsuari.Name = "TxtUsuari"
+        Me.TxtUsuari.Size = New System.Drawing.Size(213, 22)
+        Me.TxtUsuari.TabIndex = 2
         '
-        'Button2
+        'BtnAgr
         '
-        Me.Button2.Location = New System.Drawing.Point(398, 132)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(121, 38)
-        Me.Button2.TabIndex = 3
-        Me.Button2.Text = "Añadir"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.BtnAgr.Location = New System.Drawing.Point(398, 132)
+        Me.BtnAgr.Name = "BtnAgr"
+        Me.BtnAgr.Size = New System.Drawing.Size(121, 38)
+        Me.BtnAgr.TabIndex = 3
+        Me.BtnAgr.Text = "Añadir"
+        Me.BtnAgr.UseVisualStyleBackColor = True
         '
         'Button3
         '
         Me.Button3.Location = New System.Drawing.Point(29, 355)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.Size = New System.Drawing.Size(93, 60)
         Me.Button3.TabIndex = 4
-        Me.Button3.Text = "Button3"
+        Me.Button3.Text = "Ordenación numérica"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(158, 355)
+        Me.Button4.Location = New System.Drawing.Point(144, 355)
         Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.Size = New System.Drawing.Size(91, 60)
         Me.Button4.TabIndex = 5
-        Me.Button4.Text = "Button4"
+        Me.Button4.Text = "Ordenación ASCI"
         Me.Button4.UseVisualStyleBackColor = True
         '
         'RadioButton1
         '
         Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(460, 456)
+        Me.RadioButton1.Location = New System.Drawing.Point(398, 394)
         Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(33, 21)
+        Me.RadioButton1.Size = New System.Drawing.Size(105, 21)
         Me.RadioButton1.TabIndex = 6
         Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = " "
+        Me.RadioButton1.Text = "Descendent"
         Me.RadioButton1.UseVisualStyleBackColor = True
         '
         'Label1
@@ -104,26 +105,27 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Location = New System.Drawing.Point(98, 310)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(51, 17)
+        Me.Label1.Size = New System.Drawing.Size(130, 17)
         Me.Label1.TabIndex = 7
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Tipus de ordenació"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
         Me.Label2.Location = New System.Drawing.Point(29, 28)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(51, 17)
+        Me.Label2.Size = New System.Drawing.Size(93, 17)
         Me.Label2.TabIndex = 8
-        Me.Label2.Text = "Label2"
+        Me.Label2.Text = "Introduce las "
         '
-        'ComboBox1
+        'ComboObligatorio
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(352, 92)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(213, 24)
-        Me.ComboBox1.TabIndex = 9
+        Me.ComboObligatorio.FormattingEnabled = True
+        Me.ComboObligatorio.Items.AddRange(New Object() {"Principio List Box", "Ordenación Numerica", "Final List BoX"})
+        Me.ComboObligatorio.Location = New System.Drawing.Point(352, 92)
+        Me.ComboObligatorio.Name = "ComboObligatorio"
+        Me.ComboObligatorio.Size = New System.Drawing.Size(213, 24)
+        Me.ComboObligatorio.TabIndex = 9
         '
         'Button5
         '
@@ -150,16 +152,17 @@ Partial Class Form1
         Me.ClientSize = New System.Drawing.Size(830, 575)
         Me.Controls.Add(Me.Button6)
         Me.Controls.Add(Me.Button5)
-        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.ComboObligatorio)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.RadioButton1)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.BtnAgr)
+        Me.Controls.Add(Me.TxtUsuari)
+        Me.Controls.Add(Me.Lista)
         Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
@@ -168,15 +171,15 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Button1 As Button
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button2 As Button
+    Friend WithEvents Lista As ListBox
+    Friend WithEvents TxtUsuari As TextBox
+    Friend WithEvents BtnAgr As Button
     Friend WithEvents Button3 As Button
     Friend WithEvents Button4 As Button
     Friend WithEvents RadioButton1 As RadioButton
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboObligatorio As ComboBox
     Friend WithEvents Button5 As Button
     Friend WithEvents Button6 As Button
 End Class
--- practica3_interficies/practica3_interficies/Form1.vb
+++ practica3_interficies/practica3_interficies/Form1.vb
@@ -1,9 +1,9 @@
 ﻿Public Class Form1
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private Sub BtnAgr_Click(sender As Object, e As EventArgs) Handles BtnAgr.Click
 
     End Sub
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ComboObligatorio.SelectedIndex = 1
     End Sub
 End Class
