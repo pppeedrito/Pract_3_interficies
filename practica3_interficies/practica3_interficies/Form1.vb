544cc52efac8f4066242454209db9ccf956042bd
pppeedrito <pedro1996616@gmail.com>
2016-11-14 20:13:07 +0100

    f

--- practica3_interficies/practica3_interficies/Form1.vb
+++ practica3_interficies/practica3_interficies/Form1.vb
@@ -1,10 +1,37 @@
 ﻿Public Class Form1
     Private Sub BtnAgr_Click(sender As Object, e As EventArgs) Handles BtnAgr.Click
-        If (ComboObligatorio.SelectedIndex = 0) Then
-            Lista.Items.Insert(0, Text)
-        ElseIf (ComboObligatorio.SelectedIndex = 0) Then
-            Lista.Items.Add(Text)
-        End If
+        OrdenarCombo(TxtUsuari.Text)
+
+    End Sub
+
+    Private Sub OrdenarCombo(text As String)
+        Dim numero As Integer
+        Try
+            numero = text
+        Catch ex As Exception
+            MsgBox("Error introducido malo.")
+            TxtUsuari.Clear()
+            TxtUsuari.Focus()
+
+        End Try
+        Dim i As Integer
+        i = ComboObligatorio.SelectedIndex
+        Select Case i
+            Case 0
+                Lista.Items.Insert(0, TxtUsuari.Text)
+            Case 1
+                Dim Tabla
+
+                For j As Integer = 0 To ComboObligatorio.Items.Count() - 1
+
+                Next
+                If (ComboBox.SelectedIndex) Then
+
+                End If
+            Case 2
+                Lista.Items.Add(TxtUsuari.Text)
+
+        End Select
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
